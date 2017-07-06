76fd1483d15625af13fa409e2584461e49e752fd
Kai Curry <kai@cape.io>
2017-07-06 14:28:16 -0400

    Use a reducer instead of forEach thing.

--- src/clean.js
+++ src/clean.js
@@ -1,31 +1,21 @@
-import { forEach, isDate, isEmpty, isObject, isUndefined, map, reject, trim } from 'lodash'
+import { isEmpty, isUndefined, map, reject, set, transform, trim } from 'lodash'
 
+// Assume we have a fairly normal data object here?
 export function cleanObject(obj, clean) {
-  // Assume at this point we have a fairly normal data object?
   // Build up a new so fresh, so clean object.
-  const item = {}
-  function addValue(key, val) {
-    if (!isUndefined(val)) item[key] = val
-  }
-  forEach(obj, (value, key) => {
+  function reducer(result, value, key) {
     const cleanValue = clean(value, clean)
-    if (isObject(cleanValue)) {
-      // Do we really want to compact to check if empty?
-      if (isDate(cleanValue)) return addValue(key, cleanValue)
-      if (isEmpty(cleanValue)) return undefined
-    }
-    return addValue(key, cleanValue)
-  })
-  return item
+    return isUndefined(cleanValue) ? result : set(result, key, cleanValue)
+  }
+  const item = transform(obj, reducer)
+  return isEmpty(item) ? undefined : item
 }
+
 export function cleanString(string) {
-  const result = trim(string)
-  if (result === '') return undefined
-  return result
+  return trim(string) || undefined
 }
 export function cleanArray(items, clean) {
   const cleanItems = map(items, item => clean(item, clean))
   const remainingItems = reject(cleanItems, isUndefined)
-  if (isEmpty(remainingItems)) return undefined
-  return remainingItems
+  return isEmpty(remainingItems) ? undefined : remainingItems
 }
